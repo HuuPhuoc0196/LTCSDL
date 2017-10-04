9455698b1fb07b847c4fe1ebe2f2decd2c4ee37e
Huu Phuoc <lehuuphuoc0196@gmail.com>
2017-10-04 09:15:01 +0700

    Add BaiTap_Buoi3

---
+++ BaiTap_Buoi3/BaiTap_Buoi3/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace BaiTap_Buoi3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(33, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 145);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loại Sản Phẩm";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã loại: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên loại: ";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(149, 53);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(193, 30);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(149, 91);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(263, 30);
+            this.txtName.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnInsert);
+            this.panel1.Location = new System.Drawing.Point(33, 191);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 62);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(37, 14);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(96, 32);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Thêm";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(174, 15);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(96, 32);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(316, 15);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(96, 32);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Location = new System.Drawing.Point(33, 281);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.Size = new System.Drawing.Size(444, 361);
+            this.dgvShow.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 669);
+            this.Controls.Add(this.dgvShow);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Loại Sản Phẩm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dgvShow;
+    }
+}
+
---
+++ BaiTap_Buoi3/BaiTap_Buoi3/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BaiTap_Buoi3
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection cn = null;
+        DataSet dataset = null;
+        String sqlSelect;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String cnStr = "Server = .; Database = QLBanHang; Integrated Security = true;";
+            cn = new SqlConnection(cnStr);
+            sqlSelect = "SELECT * FROM LoaiSP";
+           // dgvShow.DataSource = GetDataSet(sql).Tables[0];
+            dgvShow.DataSource = GetData(sqlSelect);
+        }
+        private void Connect()
+        {
+            if (cn != null && cn.State != ConnectionState.Open)
+                cn.Open();
+        }
+
+        private void DisConnect()
+        {
+            if (cn != null && cn.State != ConnectionState.Closed)
+                cn.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            String sql = "DELETE FROM LoaiSP WHERE MaLoaiSP = '" + txtID.Text.Trim() + "'";
+            Connect();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            int numberOfRosw = 0;
+            try
+            {
+                numberOfRosw = cmd.ExecuteNonQuery();
+
+                dgvShow.DataSource = GetData(sqlSelect);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Không thể xóa dòng đang bị khóa (locked). \n\n" + ex.Message, "Thông báo");
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Kết nối đã đóng hoặc mất kết nối. \n\n" + ex.Message, "Thông báo");
+            }
+            finally
+            {
+                DisConnect();
+                txtID.Text = txtName.Text = "";
+            }
+            if(numberOfRosw != 0)
+                MessageBox.Show("Xóa thành công \n\nSố dòng đã xóa là: " + numberOfRosw.ToString() , "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                MessageBox.Show("Mã Loại không đúng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            txtID.Text = txtName.Text = "";
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            String sql = "INSERT INTO LoaiSP VALUES ('" + txtID.Text.Trim() + "', N'" + txtName.Text.Trim() + "')";
+            Connect();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            int numberOfRosw = 0;
+            try
+            {
+                numberOfRosw = cmd.ExecuteNonQuery();
+                dgvShow.DataSource = GetData(sqlSelect);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi. \n\n" + ex.Message, "Thông báo");
+            }
+            finally
+            {
+                DisConnect();
+                txtID.Text = txtName.Text = "";
+            }
+            if (numberOfRosw != 0)
+                MessageBox.Show("Thêm loại sản phẩm thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                MessageBox.Show("Thêm khong thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            txtID.Text = txtName.Text = "";
+        }
+
+        private DataSet GetDataSet(String sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql,cn);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+            return ds;
+        }
+
+        private List<Object> GetData(String sql)
+        {
+            Connect();
+
+            List<Object> list = new List<Object>();
+            int maSP;
+            String tenSP;
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    maSP = dr.GetInt32(0);
+                    tenSP = dr.GetString(1);
+
+                    var pro = new
+                    {
+                        MaSanPham = maSP,
+                        TenSanPham = tenSP
+                    };
+                    list.Add(pro);
+                }
+                dr.Close();
+            }
+            catch (SqlException ex) // Internet => Exception
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                DisConnect();
+            }
+            return list;
+        }
+
+    }
+}
