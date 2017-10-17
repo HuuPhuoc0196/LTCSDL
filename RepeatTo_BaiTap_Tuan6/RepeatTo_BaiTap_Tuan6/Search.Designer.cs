2ea976d2b5ac0f338e0fbc047ff71e5c1f4007e5
Huu Phuoc <lehuuphuoc0196@gmail.com>
2017-10-17 16:37:59 +0700

    Add RepeatTo_BaiTap_Tuan6 complete and SHOPONLINE.sql

---
+++ RepeatTo_BaiTap_Tuan6/RepeatTo_BaiTap_Tuan6/Search.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RepeatTo_BaiTap_Tuan6
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.rdStatus = new System.Windows.Forms.RadioButton();
+            this.rdName = new System.Windows.Forms.RadioButton();
+            this.rdId = new System.Windows.Forms.RadioButton();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.rdStatus);
+            this.groupBox1.Controls.Add(this.rdName);
+            this.groupBox1.Controls.Add(this.rdId);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox1.Location = new System.Drawing.Point(35, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(623, 188);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tìm kiếm Khách Hàng";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(207, 136);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(135, 37);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Tìm";
+            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // rdStatus
+            // 
+            this.rdStatus.AutoSize = true;
+            this.rdStatus.Location = new System.Drawing.Point(368, 85);
+            this.rdStatus.Name = "rdStatus";
+            this.rdStatus.Size = new System.Drawing.Size(250, 33);
+            this.rdStatus.TabIndex = 4;
+            this.rdStatus.TabStop = true;
+            this.rdStatus.Text = "Tìm theo Tình Trạng";
+            this.rdStatus.UseVisualStyleBackColor = true;
+            // 
+            // rdName
+            // 
+            this.rdName.AutoSize = true;
+            this.rdName.Location = new System.Drawing.Point(179, 85);
+            this.rdName.Name = "rdName";
+            this.rdName.Size = new System.Drawing.Size(175, 33);
+            this.rdName.TabIndex = 3;
+            this.rdName.TabStop = true;
+            this.rdName.Text = "Tìm theo Tên";
+            this.rdName.UseVisualStyleBackColor = true;
+            // 
+            // rdId
+            // 
+            this.rdId.AutoSize = true;
+            this.rdId.Checked = true;
+            this.rdId.Location = new System.Drawing.Point(5, 85);
+            this.rdId.Name = "rdId";
+            this.rdId.Size = new System.Drawing.Size(165, 33);
+            this.rdId.TabIndex = 2;
+            this.rdId.TabStop = true;
+            this.rdId.Text = "Tìm theo Mã";
+            this.rdId.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(80, 34);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(446, 35);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập";
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Location = new System.Drawing.Point(35, 206);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.Size = new System.Drawing.Size(623, 240);
+            this.dgvShow.TabIndex = 6;
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 458);
+            this.Controls.Add(this.dgvShow);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Search";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.Search_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RadioButton rdStatus;
+        private System.Windows.Forms.RadioButton rdName;
+        private System.Windows.Forms.RadioButton rdId;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ RepeatTo_BaiTap_Tuan6/RepeatTo_BaiTap_Tuan6/Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace RepeatTo_BaiTap_Tuan6
+{
+    public partial class Search : Form
+    {
+        private SqlConnection cnn = null;
+        public Search()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            if(rdId.Checked){
+                string id = txtInput.Text;
+                sql = "SELECT * FROM KHACHHANG WHERE MaKhachHang = '" + id + "'";
+            }else if(rdName.Checked){
+                string name = txtInput.Text;
+                sql = "SELECT * FROM KHACHHANG WHERE HoTen LIKE '%" + name + "%'";
+            }
+            else
+            {
+                string status = txtInput.Text;
+                sql = "SELECT * FROM KHACHHANG WHERE TinhTrang = '" + status + "'";
+            }
+            dgvShow.DataSource = new DataCustomer().GetData(cnn, sql);
+        }
+
+        private void Search_Load(object sender, EventArgs e)
+        {
+            string cnStr = ConfigurationManager.ConnectionStrings["CustomerConnectString"].ConnectionString;
+            cnn = new SqlConnection(cnStr);
+        }
+    }
+}
